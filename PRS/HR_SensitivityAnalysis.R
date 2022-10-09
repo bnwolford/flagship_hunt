9dc4fd53b6cf083964c321e7860f8405d15c8b8f
bnwolford <bwolford@umich.edu>
2022-10-09 22:01:42 +0000

    Working on custom meta analysis script with plotting

--- PRS/HR_SensitivityAnalysis.R
+++ PRS/HR_SensitivityAnalysis.R
@@ -1,1 +1,16 @@
 #sensitivity analysis for incident and prevalent cases in HUNT 
+
+
+
+#we should be able to simply take the HRperSD script, add into Surv model Surv(age at recruitment, age at diagnosis, event)~...., and simply run the the analysis, as model will exclude everyone itself who has negative follow up, ie prevalent cases
+3:43
+no need to redefine anything
+3:44
+exepct the surv object as far as i can understand
+3:45
+and then we can make a graph on HR from agefodgn time scale and HR on entry,ageofdgn scale
+
+
+
+
+
